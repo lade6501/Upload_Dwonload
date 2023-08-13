3c0452074e175e3ad5a7a0cf91b2fd20177cf9cb
lade6501 <lade6501@gmail.com>
2023-08-13 23:27:03 +0530

    Added file upload view

--- src/components/fileUpload/fileUpload.jsx
+++ src/components/fileUpload/fileUpload.jsx
@@ -4,6 +4,17 @@ const FileUpload = () => {
   return (
     <>
       <h1>Files Upload/Download </h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="formFile" className="form-label">
+            Please choose file/files to upload
+          </label>
+          <input className="form-control" type="file" id="formFile" />
+        </div>
+        <button type="button" className="btn btn-primary">
+          Upload
+        </button>
+      </form>
     </>
   );
 };
