8c79578e1ee9c12ff57e4713ff7f544c1c1f5f4e
lade6501 <lade6501@gmail.com>
2023-08-14 23:05:33 +0530

    Added table for files

--- src/components/fileTable/fileTable.jsx
+++ src/components/fileTable/fileTable.jsx
@@ -4,7 +4,20 @@ import "./fileTable.css";
 const FileTable = () => {
   return (
     <>
-      <h1>Recent Files</h1>
+      <h1 className="tableHead">Recent Files</h1>
+
+      <table className="table table-striped" id="table">
+        <thead className="table-dark">
+          <tr>
+            <th>File Name</th>
+            <th>File Type</th>
+            <th>Size</th>
+            <th>View</th>
+            <th>Download</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+      </table>
     </>
   );
 };
