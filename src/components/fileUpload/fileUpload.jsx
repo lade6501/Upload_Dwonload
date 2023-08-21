6d7c8a172f4c93fef0fe1da5abc13a7f52551c24
lade6501 <lade6501@gmail.com>
2023-08-21 22:36:35 +0530

    Calling file upload api

--- src/components/fileUpload/fileUpload.jsx
+++ src/components/fileUpload/fileUpload.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./fileUpload.css";
+
 const FileUpload = () => {
+  const [file, setFile] = useState();
+
+  const upload = () => {
+    const data = new FormData();
+    data.append("file", file);
+
+    fetch("http://localhost:8005/api/v1/files", {
+      method: "post",
+      body: data,
+    })
+      .then(async (res) => {
+        const data = await res.json();
+        alert(data.message);
+      })
+      .catch((err) => {
+        console.error("err ", err);
+      });
+  };
+
   return (
     <>
       <h1>Files Upload/Download </h1>
@@ -9,9 +29,18 @@ const FileUpload = () => {
           <label htmlFor="formFile" className="form-label">
             Please choose file/files to upload
           </label>
-          <input className="form-control" type="file" id="formFile" />
+          <input
+            className="form-control"
+            type="file"
+            id="formFile"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
         </div>
-        <button type="button" className="btn btn-primary">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => upload()}
+        >
           Upload
         </button>
       </form>
