ddf884bbab8bcbe27a24a9bf0c2c6faf60a40993
lade6501 <lade6501@gmail.com>
2023-08-22 00:08:30 +0530

    Calling api to get all uploaded files from DB

--- src/components/fileUpload/fileUpload.jsx
+++ src/components/fileUpload/fileUpload.jsx
@@ -1,20 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./fileUpload.css";
 
 const FileUpload = () => {
   const [file, setFile] = useState();
+  const [files, setFiles] = useState([]);
 
+  useEffect(() => {
+    getFiles();
+  }, []);
+
+  //Calling api to get all uploaded files from DB
+  const getFiles = () => {
+    fetch("http://localhost:8005/api/v1/files")
+      .then(async (res) => {
+        const data = await res.json();
+        setFiles(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  //Calling an api to upload file to DB
   const upload = () => {
     const data = new FormData();
     data.append("file", file);
 
-    fetch("http://localhost:8005/api/v1/files", {
+    fetch("http://localhost:8005/api/v1/upload", {
       method: "post",
       body: data,
     })
       .then(async (res) => {
         const data = await res.json();
         alert(data.message);
+        getFiles();
       })
       .catch((err) => {
         console.error("err ", err);
