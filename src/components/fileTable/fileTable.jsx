794eb018f0341a68e79d4a718e82866373e4e122
lade6501 <lade6501@gmail.com>
2023-08-26 21:17:10 +0530

    Added file not found scenario

--- src/components/fileTable/fileTable.jsx
+++ src/components/fileTable/fileTable.jsx
@@ -6,40 +6,46 @@ import byte from "byte-size";
 const FileTable = ({ files }) => {
   return (
     <>
-      <h1 className="tableHead">Recent Files</h1>
+      {files.length == 0 ? (
+        <h1 className="tableHead">No files found</h1>
+      ) : (
+        <>
+          <h1 className="tableHead">Recent Files</h1>
 
-      <table className="table table-striped" id="table">
-        <thead className="table-dark">
-          <tr>
-            <th>File Name</th>
-            <th>File Type</th>
-            <th>Size</th>
-            <th>View</th>
-            <th>Download</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {files.map((file, index) => {
-            return (
-              <tr key={index}>
-                <td>{file.filename}</td>
-                <td>{`To be added`}</td>
-                <td>{`${byte(file.length)}`}</td>
-                <td>
-                  <i className="fa-solid fa-eye"></i>
-                </td>
-                <td>
-                  <i className="fa-solid fa-download"></i>
-                </td>
-                <td>
-                  <i className="fa-solid fa-trash"></i>
-                </td>
+          <table className="table table-striped" id="table">
+            <thead className="table-dark">
+              <tr>
+                <th>File Name</th>
+                <th>File Type</th>
+                <th>Size</th>
+                <th>View</th>
+                <th>Download</th>
+                <th>Delete</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {files.map((file, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{file.filename}</td>
+                    <td>{`To be added`}</td>
+                    <td>{`${byte(file.length)}`}</td>
+                    <td>
+                      <i className="fa-solid fa-eye"></i>
+                    </td>
+                    <td>
+                      <i className="fa-solid fa-download"></i>
+                    </td>
+                    <td>
+                      <i className="fa-solid fa-trash"></i>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
     </>
   );
 };
