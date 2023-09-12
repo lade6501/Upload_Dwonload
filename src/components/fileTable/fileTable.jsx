23365e5e8dc98346aedb9f654ee68909bff6ced9
lade6501 <lade6501@gmail.com>
2023-09-12 22:02:38 +0530

    Added filter type

--- src/components/fileTable/fileTable.jsx
+++ src/components/fileTable/fileTable.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./fileTable.css";
 import mime from "mime";
 import byte from "byte-size";
+import Dropdown from "react-bootstrap/Dropdown";
 
 const FileTable = ({ files }) => {
-  console.log(files);
   return (
     <>
       {files.length == 0 ? (
@@ -17,8 +17,35 @@ const FileTable = ({ files }) => {
             <thead className="table-dark">
               <tr>
                 <th>File Name</th>
-                <th>File Type</th>
-                <th>Size</th>
+                <th>
+                  File Type
+                  <Dropdown>
+                    <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                      <i className="fa-solid fa-filter"></i>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                      <Dropdown.Item>Images</Dropdown.Item>
+                      <Dropdown.Item>PDF'S</Dropdown.Item>
+                      <Dropdown.Item>Text</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </th>
+                <th>
+                  Size
+                  <Dropdown>
+                    <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                      <i className="fa-solid fa-filter"></i>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                      <Dropdown.Item>Smallest first</Dropdown.Item>
+                      <Dropdown.Item>Biggest first</Dropdown.Item>
+                      <Dropdown.Item>Top 5 smallest</Dropdown.Item>
+                      <Dropdown.Item>Top 5 biggest</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </th>
                 <th>View</th>
                 <th>Download</th>
                 <th>Delete</th>
